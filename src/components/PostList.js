556d3fb4cb46df68e14abc78af5dc1ec73abc20b
Michael S <mihai.sanfran@gmail.com>
2019-06-09 17:38:37 -0700

    Wire up fetchPosts action creator to PostList component

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { fetchPosts } from '../actions';
 
 class PostList extends React.Component {
+    componentDidMount(){
+        // Call action creator that comes from connect function
+        this.props.fetchPosts();
+    }
+    
     render(){
         return <div>Post List</div>;
     }
 }
 
-export default PostList;
+// Pass action creators to this component
+export default connect(null, {
+    fetchPosts // fetchPosts: fetchPosts
+})(PostList);
