07441214cfa6f95eeea0452978d65f8e8b3f4c4a
MihaiSmarandache <mihai.sanfran@gmail.com>
2019-06-23 15:38:45 -0700

    Wire up Redux-Thunk middleware to the async network request

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,13 @@
-export const fetchPosts = () => {
-    return {
-        type: 'FETCH_POSTS'
-    };
+import jsonPlaceholder from '../apis/jsonPlaceholder';
+
+// Because using async await, once it gets transpiled to es15 it is not returning a plain js object!!
+// It is returning a request object without the type property so it will error out
+// Need to applyMiddleware Redux-Thunk in index.js to solve it
+export const fetchPosts = () => async dispatch => {
+    const response = await jsonPlaceholder.get('/posts');
+
+    dispatch({
+        type: 'FETCH_POSTS',
+        payload: response
+    });
 };
