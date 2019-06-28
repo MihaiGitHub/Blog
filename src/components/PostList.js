aea4b30e4488674471a141fb5e629f9ab4f1765a
Michael S <mihai.sanfran@gmail.com>
2019-06-27 18:54:58 -0700

    Create postsReducer

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -9,11 +9,17 @@ class PostList extends React.Component {
     }
     
     render(){
+        console.log(this.props.posts)
+        
         return <div>Post List</div>;
     }
 }
 
+const mapStateToProps = (state) => {
+    return { posts: state.posts };
+}
+
 // Pass action creators to this component
-export default connect(null, {
+export default connect(mapStateToProps, {
     fetchPosts // fetchPosts: fetchPosts
 })(PostList);
